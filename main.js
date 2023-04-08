c76072fa568ecbf4d4c6ebd7011b5475c2f4300b
Zineb Amezyane <zineb.amezyane0@gmail.com>
2023-04-08 14:28:39 -0400

    js

--- main.js
+++ main.js
@@ -9,7 +9,19 @@ function fullscreen() {
         canvas.height = window.innerHeight;
         canvas.className = "fullscreen"
 
+        document.body.scrollTop = 0; // <-- pull the page back up to the top
+        document.body.style.overflow = 'hidden'; // <-- relevant addition
+    } else {
+        fullWindowState = false;
+        //canvas goes normal
+        canvas.width = 820;
+        canvas.height = 600;
+        canvas.className = "";
+
+        document.body.style.overflow = 'visible'; // <-- toggle back to normal mode
     }
+
+}
 
 }
 
